de030e53380504f3b48e8edad12fb2e4993c604d
gitsnippet <pengtao@hncfs.edu.cn>
2018-11-28 08:19:18 +0800

    add files at  Wed Nov 28 08:19:18 HKT 2018

--- add.php
+++ add.php
@@ -1,14 +1,21 @@
 <?php
-extract($_POST);
+$date = trim($_POST["date"]);
+$class = trim($_POST["class"]);
+$process = trim($_POST["process"]);
+
 $dsn = "mysql:host=localhost;dbname=learningProcess";
 $con = new PDO($dsn,"root","xyzzy");
+if($date == ""){
+    $date = date("Y-m-d");
+}
+
 $query ="insert into process(`time`,`class`,`process`) 
              values('$date','$class','$process')"; 
 if($con->query($query)){
-        echo "成功写入数据库";
+    echo "成功写入数据库";
 }
 else{
-        echo "something wrong";
+    echo "something wrong";
 }
 ?>
 
--- learningProcess.sql
+++ learningProcess.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE `learningProcess`;
+USE `learningProcess`;
 CREATE TABLE `process` (
       `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
       `time` date NOT NULL COMMENT '日期',
       `class` varchar(20) NOT NULL COMMENT '课程的名称',
-      `process` varchar(5) NOT NULL COMMENT '课程的进度',
+      `process` varchar(100) NOT NULL COMMENT '课程的进度',
       PRIMARY KEY (`id`),
       UNIQUE KEY  (`id`,`time`,`class`,`process`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
